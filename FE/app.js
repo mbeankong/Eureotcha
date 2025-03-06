a609fc481a22720d0f29bf97966ef566437dafd5
LeeSeHyeon <155310328+SeHyeonLee-dev@users.noreply.github.com>
2025-03-06 14:12:17 +0900

    Revert "index.html, index.css 뒤로가기 추가"
    This reverts commit fa1abe3408b048fb89df70d11f88edf644aadec8.

--- FE/app.js
+++ FE/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -9,14 +8,7 @@ app.get("/api/message", (req, res) => {
   res.json({ message: "FE 서버 시작 !" });
 });
 
-// HTML 서빙
-app.get("/orders", (req, res) => {
-  res.sendFile(path.join(__dirname, "src/html", "orders.html"));
-});
-
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-
