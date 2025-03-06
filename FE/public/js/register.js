d0d45b700d29c46a30728601852117fd5c3b668a
Gdugboy <gdugboy@gmail.com>
2025-03-06 23:30:11 +0900

    등록버튼 누르면 BE json에 입력 price는 개별구매가격

--- FE/public/js/register.js
+++ FE/public/js/register.js
@@ -21,6 +21,45 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
+    // ✅ 등록하기 버튼 클릭 시 데이터 전송
+    registerBtn.addEventListener("click", async function () {
+        const product = document.getElementById("item-name").value;
+        const totalAmount = parseInt(document.getElementById("total-quantity").value);
+        const targetCount = parseInt(document.getElementById("num-people").value);
+        const productUrl = document.getElementById("product-link").value;
+        const wholePrice = parseInt(document.getElementById("total-price").value);
+        const pricePerson = wholePrice / targetCount ;
+
+        const newGroupBuy = {
+            product: product,
+            total_amount: totalAmount,
+            image_url: "uploads/default.png", // 기본 이미지
+            current_count: 1, // 개설자는 자동 참여
+            target_count: targetCount,
+            price: pricePerson,
+            product_url: productUrl,
+            status: "open"
+        };
+
+        try {
+            const response = await fetch("http://localhost:4000/api/group-buy", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newGroupBuy)
+            });
+
+        //     if (response.ok) {
+        //         alert("공동구매가 성공적으로 등록되었습니다.");
+        //     } else {
+        //         alert("등록에 실패했습니다.");
+        //     }
+        } catch (error) {
+            console.error("등록 중 오류 발생:", error);
+        }
+    });
+
     // 등록하기 버튼 클릭 시 모달창 열기
     registerBtn.addEventListener("click", function () {
         modal.style.display = "flex";
