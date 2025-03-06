7fb3ca1b16e70fe3364c29f711a2de56dde5dc18
민항섭 <minhangseob@MacBook-Air.local>
2025-03-07 01:31:40 +0900

    동적데이터

--- FE/public/js/gongu_join.js
+++ FE/public/js/gongu_join.js
@@ -1,6 +1,9 @@
 function goBack() {
   window.history.back();
 }
+const pathParts = window.location.pathname.split('/');
+const id = pathParts[pathParts.length - 1];
+const gongu_detail = document.getElementById('gongu_join_detail');
 
 document.addEventListener('DOMContentLoaded', function () {
   const modal = document.getElementById('modal-container');
@@ -16,4 +19,28 @@ document.addEventListener('DOMContentLoaded', function () {
   cancelBtn.addEventListener('click', function () {
     modal.style.display = 'none';
   });
+});
+
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const response = await fetch(`http://localhost:4000/api/group-buy/${id}`);
+    const groupBuy = await response.json();
+    gongu_detail.innerHTML = `
+    <div class="gongu_join_item">${groupBuy.product}</div>
+        <div class="gongu_join_image">
+          <img src='${groupBuy.image_url}' alt="" />
+        </div>
+        <div class="gongu_join_info">
+          <div>내가 가질 개수 : ${
+            groupBuy.total_amount / groupBuy.target_count
+          }개</div>
+
+          <div>1/${groupBuy.target_count} 명</div>
+
+          <div>내가 지불할 금액 : ${groupBuy.price}원</div>
+          <div>상품 URL : ${groupBuy.product_url}</div>
+        </div>`;
+  } catch (error) {
+    console.error('데이터를 불러오는 중 오류 발생 : ', error);
+  }
 });
