fa1abe3408b048fb89df70d11f88edf644aadec8
LeeSeHyeon <155310328+SeHyeonLee-dev@users.noreply.github.com>
2025-03-06 13:58:52 +0900

    index.html, index.css 뒤로가기 추가

--- FE/app.js
+++ FE/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -8,7 +9,14 @@ app.get("/api/message", (req, res) => {
   res.json({ message: "FE 서버 시작 !" });
 });
 
+// HTML 서빙
+app.get("/orders", (req, res) => {
+  res.sendFile(path.join(__dirname, "src/html", "orders.html"));
+});
+
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
